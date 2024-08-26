2d549d462a060885d9263c87f39c997d100aa71a
Dean Zhao <rcarlosdasilva@qq.com>
2024-08-26 10:53:57 +0800

    fix: meta info

--- src/main/kotlin/cn/labzen/tool/meta/ToolMeta.kt
+++ src/main/kotlin/cn/labzen/tool/meta/ToolMeta.kt
@@ -5,11 +5,11 @@ import cn.labzen.meta.component.LabzenComponent
 class ToolMeta : LabzenComponent {
 
   override fun description(): String =
-    "Labzen.Tool"
+    "工具包，包含最基本的包依赖集合，并提供普适基础工具与底层功能"
 
   override fun mark(): String =
-    "cn.labzen.tool"
+    "Labzen-Tool"
 
   override fun packageBased(): String =
-    "工具包，包含最基本的包依赖集合，并提供普适基础工具与底层功能"
+    "cn.labzen.tool"
 }
